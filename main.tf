b99f064e40ea4fce4e85534ebbd72fd3cf6c3cad
mvpc-11232 <mvp2612@hotmail.com>
2022-12-22 07:51:26 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 # This should only be called for the frontend component
 module "alb-public" {
   source                   = "./vendor/modules/alb" 
-  ALB_NAME                 = "roboshop-" 
+  ALB_NAME                 = "roboshop-public" 
 }
 
 # This should only be called for the backend component
 module "alb-private" {
   source                   = "./vendor/modules/alb"  
+    ALB_NAME                 = "roboshop-private" 
 }
 
