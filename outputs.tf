3c4f14bc728794d0055d8c1922fae56a96f6a90e
CodingManoj <mvp2612@hotmail.com>
2022-12-25 08:40:45 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "PUBLIC_ALB_ARN" {
 output "PRIVATE_ALB_ARN" {
     value  = module.alb-private.ALB_ARN
 }
+
+output "PRIVATE_LISTENER_ARN" {
+    value = module.alb-private.
+    value = aws_lb_listener.private.*.arn   # * is coming to picture as we are using count.
+}
